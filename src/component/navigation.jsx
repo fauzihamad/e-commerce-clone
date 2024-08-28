50a9eed4371f6c6c17117d9b3f91bde2a03e4178
fauzihamad <fauzihamad23@gmail.com>
2024-08-29 00:52:58 +0700

    create navigation

--- src/component/navigation.jsx
+++ src/component/navigation.jsx
@@ -1,19 +1,45 @@
 import '../assets/index.css'
 
 import logo from '../assets/img/logo.svg';
-import SearchIcon from '../assets/img/search-icon.svg';
+import AddShoppingCartIcon from '@mui/icons-material/AddShoppingCart';
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+import TextField from '@mui/material/TextField';
+import InputAdornment from '@mui/material/InputAdornment';
+import SearchIcon from '@mui/icons-material/Search';
 
 const Navigation = () => {
   return (
-    <div className="bg-red flex gap-6 mx-32 w-full items-center py-6">
+    <div className="bg-sky-400 flex gap-12 px-32 w-full items-center py-6 whitespace-nowrap">
       <img src={logo} alt="Logo" />
       <p> Shop </p>
       <p> On Sale </p>
       <p> New Arrival </p>
       <p> Brands </p>
-      <div className='bg-[#F0F0F0] rounded-[62px] p-3 flex gap-4 flex-shrink'>
-        <img src={SearchIcon} />
-        <p> Search for a product ....</p>
+      <TextField
+        placeholder="Search for a product ...."
+        variant="outlined"
+        fullWidth
+        InputProps={{
+          startAdornment: (
+            <InputAdornment position="start">
+              <SearchIcon />
+            </InputAdornment>
+          ),
+          sx: {
+            borderRadius: '62px', // Apply 62px border-radius to the input field
+            backgroundColor: '#F0F0F0', // Apply background color
+            paddingLeft: '10px',
+            flexGrow:1 // Adjust padding if necessary
+          },
+        }}
+        sx={{
+          borderRadius: '62px',
+          // maxWidth: '600px',
+        }}
+      />
+      <div className='flex gap-2'>
+        <AddShoppingCartIcon />
+        <AccountCircleIcon />
       </div>
     </div>
   );
